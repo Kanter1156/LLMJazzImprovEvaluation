d675df29050e0e05ef73e399c3c5abe7d36bb8ec
Kanter1156 <nedderlanderson@gmail.com>
2026-08-06 10:35:33 -0700

    Update and rename notesegmentation.py to load_audio.py

---
+++ audio/load_audio.py
@@ -0,0 +1,9 @@
+import librosa
+
+def load_audio(audio_path, slowdown_factor):
+    y, sr = librosa.load(audio_path, sr=None)
+
+    if slowdown_factor < 1.0:
+        y = librosa.effects.time_stretch(y, rate=slowdown_factor)
+
+    return y, sr
