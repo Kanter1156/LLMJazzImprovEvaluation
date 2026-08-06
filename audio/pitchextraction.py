bf981c255062a9ce88caef5a0304bcfa434fe746
Kanter1156 <nedderlanderson@gmail.com>
2026-08-06 10:35:55 -0700

    Update pitchextraction.py

--- audio/pitchextraction.py
+++ audio/pitchextraction.py
@@ -1,1 +1,15 @@
+import crepe
+from scipy.signal import medfilt
 
+def extract_pitch(y, sr, step_ms):
+    time, freq, conf, _ = crepe.predict(
+        y,
+        sr,
+        model_capacity="small",
+        step_size=step_ms,
+        viterbi=True,
+    )
+
+    freq = medfilt(freq, kernel_size=3)
+
+    return time, freq, conf
